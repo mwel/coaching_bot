571f39e072a5cb796e37024decb9d3b0a7e2135f
mwel <max.wellenhofer@gmail.com>
2021-11-22 19:06:00 +0100

    improved FTP Connector

--- bot/file-server_connector/ftp_connector.py
+++ bot/file-server_connector/ftp_connector.py
@@ -1,4 +1,18 @@
 import ftplib
 import constants.FTP_constant
 
-with ftplib.FTP_TLS(constants.FTP_constant.FTP_HOST, constants.FTP_constant.FTP_USER, constants.FTP_constant.FTP_PW ) as ftp:
+#connect to FTP Server
+def connectFTP(ftpHost, ftpUser, ftpPW):
+        with ftplib.FTP(constants.FTP_constant.FTP_HOST, constants.FTP_constant.FTP_USER, constants.FTP_constant.FTP_PW ) as ftp:
+        print('OK - FTP Connection established.')
+
+# Check for files on FTP Server
+def connectFTP(ftpHost, ftpUser, ftpPW):
+        with ftplib.FTP(constants.FTP_constant.FTP_HOST, constants.FTP_constant.FTP_USER, constants.FTP_constant.FTP_PW ) as ftp:
+            print(ftp.dir())
+
+
+
+
+
+
