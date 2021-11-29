b06b6be55c9e0829a37e7f135e5bf7006cf2b875
mwel <max.wellenhofer@gmail.com>
2021-11-29 18:37:22 +0100

    implemented user dictionary in start.py

--- bot/handler_functions/start.py
+++ bot/handler_functions/start.py
@@ -12,10 +12,22 @@ other = 'I am a unicorn.'
 
 # Starts the conversation and asks the user about their gender.
 def start(update: Update, context: CallbackContext) -> int:
-    reply_keyboard = [[female, male, other]]
 
+    # first name
+    first_name = update.message.from_user.first_name
+
+    # write first_name to dict
+    context.user_data['first_name'] = first_name
+
+    # last name
+    last_name = update.message.from_user.last_name
+    # safe more initial variables about the user here.
+    
+    print ('//printing user data: \n' + str(context.user_data))
+
+    reply_keyboard = [[female, male, other]]
     update.message.reply_text(
-        'Hi! I am The Coaching Bot by wavehoover. You have taken the first step in you journey to success '
+        f'Hi {context.user_data["first_name"]},\n' 'I am The Coaching Bot by wavehoover. You have taken the first step in you journey to success '
         'by contacting me. As your personal CoachingBot, I will guide you through the onboarding process for '
         'your first coaching session. '
         '(You can send /cancel at any time, if you are no longer interested in a conversation with me '
