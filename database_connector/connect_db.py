7fe566a08baf121a33711675bdf23c4f3c51002f
mwel <max.wellenhofer@gmail.com>
2022-01-03 16:50:42 +0100

    refer to sql file instead of hardcoding

--- database_connector/connect_db.py
+++ database_connector/connect_db.py
@@ -1,5 +1,6 @@
 # connect to the coachingBot_DB and 
 import sqlite3
+import database_connector
   
 # connecting to the database
 connection = sqlite3.connect("coachingBotDB.db")
@@ -9,16 +10,11 @@ cursor = connection.cursor()
 # print with no errors
 print("Successfully connected to coachingBotDB")
 
-# SQL command to create a table in the database
-sql_command = """CREATE TABLE emp ( 
-staff_number INTEGER PRIMARY KEY, 
-fname VARCHAR(20), 
-lname VARCHAR(30), 
-gender CHAR(1), 
-joining DATE);"""
+# if none exists, create table in database
+table_users = str(database_connector.CREATE_user_table.sql)
   
 # execute the statement
-cursor.execute(sql_command)
+cursor.execute(table_users)
 
 # close the connection
 connection.close()
