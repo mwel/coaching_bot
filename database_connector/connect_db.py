9d86aa2674db4fcfa1162602db74079ab986a547
mwel <max.wellenhofer@gmail.com>
2022-01-04 11:34:44 +0100

    fix type that errored if table already existed

--- database_connector/connect_db.py
+++ database_connector/connect_db.py
@@ -1,18 +1,18 @@
 # connect to the coachingBot_DB and check, if the user table already exists. If not, create it.
 import sqlite3
-  
+
 # connecting to the database
 connection = sqlite3.connect("coachingBotDB.db")
 print("+++++ Connected to coachingBotDB. +++++")
 
 # cursor
 cursor = connection.cursor()
-print("+++++ Curser created. +++++")
+print("+++++ Cursor created. +++++")
 
 # put table checker sql statement into a variable
 checker = '''SELECT count(name) 
     FROM sqlite_master 
-    WHERE type='table' AND name='user_table' 
+    WHERE type='table' AND name='users' 
 '''
 cursor.execute(checker)
 
@@ -33,10 +33,10 @@ table_users = '''CREATE TABLE users (
 
 #if count is 1, table already exists - else, create it and tell me you did!
 if cursor.fetchone()[0]==1: 
-	print('+++++ Table already exists. +++++')
+	print('+++++ Table "users" already exists. +++++')
 else:
     cursor.execute(table_users)
-    print('+++++ Table "table_users" created. +++++')  
+    print('+++++ Table "users" created. +++++')  
 
 #commit changes to db			
 connection.commit()
