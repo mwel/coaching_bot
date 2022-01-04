979b980b031757f0203c62523ba6cfc30bb4fd32
mwel <max.wellenhofer@gmail.com>
2022-01-04 21:35:26 +0100

    fix cancel function

--- bot/handler_functions/cancel.py
+++ bot/handler_functions/cancel.py
@@ -12,8 +12,8 @@ def cancel(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info(f'User {user.first_name} canceled the conversation.')
     update.message.reply_text(
-        'APPLICATION TERMINATED by ' + user.first_name,
-        'You ended the converstation and now have 2 options. Leave OR /start over.', reply_markup=ReplyKeyboardRemove()
+        f'APPLICATION TERMINATED by {user.first_name}\n\n'
+        'You ended the converstation and now have 2 options. Leave OR /start over.'
     )
 
     return ConversationHandler.END
--- bot/main.py
+++ bot/main.py
@@ -54,19 +54,17 @@ def main() -> None:
     # Gets the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
+    # this is the STATE MACHINE
     # Adds conversation handler with the states GENDER, PHOTO, LOCATION and BIO for stage 1 of the sign up
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            states.GENDER: [MessageHandler(Filters.regex('^(Gentleman|Lady|I am a unicorn.)$'), gender)],
-            states.PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            states.LOCATION: [
-                MessageHandler(Filters.location, location),
-                CommandHandler('skip', skip_location),
-            ],
-            states.BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            states.GENDER:      [MessageHandler(Filters.regex('^(Gentleman|Lady|I am a unicorn.)$'), gender)],
+            states.PHOTO:       [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            states.LOCATION:    [MessageHandler(Filters.location, location), CommandHandler('skip', skip_location)],
+            states.BIO:         [MessageHandler(Filters.text & ~Filters.command, bio)],
+            # more states here...
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
     )
 
     dispatcher.add_handler(conv_handler) #calling Handler from separate class
