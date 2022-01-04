3f7812dd595950f2d9e7031f0ab254bc5c6514e3
mwel <max.wellenhofer@gmail.com>
2022-01-04 22:47:47 +0100

    fix insert update command

--- database_connector/insert_update_db.py
+++ database_connector/insert_update_db.py
@@ -1,10 +1,8 @@
 # connect to the coachingBot_DB and check, if the user table already exists. If not, create it.
 import sqlite3
-from telegram import Update
-from telegram.ext import CallbackContext
 
 
-def insert_update (update: Update, user_id, first_name, last_name, gender, photo, birthdate, email, phone, longitude, latitude, bio):
+def insert_update (user_id, first_name, last_name, gender, photo, birthdate, email, phone, longitude, latitude, bio):
     # connect to db
     connection = sqlite3.connect("coachingBotDB.db")
     print("+++++ Connected to coachingBotDB. +++++")
@@ -13,47 +11,68 @@ def insert_update (update: Update, user_id, first_name, last_name, gender, photo
     cursor = connection.cursor()
     print("+++++ Cursor created. +++++")
 
-    # sql command to check, if user exists
-    user_check = f"""EXISTS (SELECT 1 FROM coachingBotDB.users WHERE user_id = {user_id}"""
     
     # sql command to INSERT a new record into the db
-    insert_command = f"""INSERT INTO coachingBotDB.users (user_id, first_name, last_name, gender, photo, birthdate, email, phone, longitude, latitude, bio) 
-            VALUES(
-            {user_id},
-            {first_name},
-            {last_name},
-            {gender},
-            {photo},
-            {birthdate},
-            {email},
-            {phone},
-            {longitude},
-            {latitude},
-            {bio},
-            )"""
+    # c.execute("INSERT INTO test VALUES (?, 'bar')", (testfield,))
+    # insert_command = 'INSERT INTO users VALUES (user_id, first_name, last_name, gender, photo, birthdate, email, phone, longitude, latitude, bio)'
+    insert_command = 'INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+    insert_args = (
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        photo,
+        birthdate,
+        email,
+        phone,
+        longitude,
+        latitude,
+        bio,
+        )
+    
 
     # sql command to UPDATE an existing record
-    update_command = f"""UPDATE coachingBotDB.users SET 
-        first_name = {first_name},
-        last_name = {last_name},
-        gender = {gender},
-        photo = {photo},
-        birthdate = {birthdate}, 
-        email = {email},
-        phone = {phone},
-        longitude = {longitude}, 
-        latitude = {latitude},
-        bio = {bio} 
-        WHERE user_id = {user_id}
+    update_command = """UPDATE users SET 
+        first_name = ?,
+        last_name = ?,
+        gender = ?,
+        photo = ?,
+        birthdate = ?, 
+        email = ?,
+        phone = ?,
+        longitude = ?, 
+        latitude = ?,
+        bio = ? 
+        WHERE user_id = ?
         """
+    update_args = (
+            first_name,
+            last_name,
+            gender,
+            photo,
+            birthdate,
+            email,
+            phone,
+            longitude,
+            latitude,
+            bio,
+            user_id)
 
 
     #if record exists, UPDATE it. Else, INSERT.
-    if cursor.execute(user_check):
-        cursor.execute(update_command)
+    # sql command to check, if user exists
+    user_check = f'SELECT EXISTS (SELECT 1 FROM users WHERE user_id={user_id})'
+
+    cursor.execute(user_check)
+
+    if cursor.fetchone():
+        print("+++++ Record Found! +++++")
+        cursor.execute(update_command, update_args)
         print (f'UPDATED record {user_id}: {first_name} {last_name}')
+
     else:
-        cursor.execute(insert_command)
+        print("+++++ No record found +++++")
+        cursor.execute(insert_command, insert_args)
         print (f'INSERTED record {user_id}: {first_name} {last_name}')
 
 
---
+++ database_connector/test_connect_db.py
@@ -0,0 +1,16 @@
+# data base tests
+import connect_db, select_db, insert_update_db, delete_record_db
+
+insert_update_db.insert_update(
+    user_id=123456789, 
+    first_name='Michael', 
+    last_name='Müller', 
+    gender='male', 
+    photo='01010101010101010101', 
+    birthdate='19900101', 
+    email='test@gmx.de', 
+    phone='+41123456789', 
+    longitude='65432345678.00', 
+    latitude='65432345678.00', 
+    bio = 'Ich wurde als Phritte gebohren. Doch also selche krepieren soll ich nicht!'
+    )
