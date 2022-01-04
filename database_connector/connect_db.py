48c15d114a66aafbe4064c68f1eb1e47dc58a7e8
mwel <max.wellenhofer@gmail.com>
2022-01-04 11:26:35 +0100

    create db and users table, if does not exist

--- database_connector/connect_db.py
+++ database_connector/connect_db.py
@@ -1,21 +1,48 @@
-# connect to the coachingBot_DB and 
+# connect to the coachingBot_DB and check, if the user table already exists. If not, create it.
 import sqlite3
-import database_connector
   
 # connecting to the database
 connection = sqlite3.connect("coachingBotDB.db")
-  
+print("+++++ Connected to coachingBotDB. +++++")
+
 # cursor
 cursor = connection.cursor()
-# print with no errors
-print("Successfully connected to coachingBotDB")
+print("+++++ Curser created. +++++")
 
-# if none exists, create table in database
-table_users = str(database_connector.CREATE_user_table.sql)
-  
-# execute the statement
-cursor.execute(table_users)
+# put table checker sql statement into a variable
+checker = '''SELECT count(name) 
+    FROM sqlite_master 
+    WHERE type='table' AND name='user_table' 
+'''
+cursor.execute(checker)
 
-# close the connection
+# put table creation sql statement into a variable
+table_users = '''CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    gender TEXT,
+    photo BLOB,
+    birthdate INT,
+    email TEXT UNIQUE,
+    phone TEXT UNIQUE,
+    longitude INT,
+    latitude INT,
+    bio TEXT NOT NULL
+);'''
+
+#if count is 1, table already exists - else, create it and tell me you did!
+if cursor.fetchone()[0]==1: 
+	print('+++++ Table already exists. +++++')
+else:
+    cursor.execute(table_users)
+    print('+++++ Table "table_users" created. +++++')  
+
+#commit changes to db			
+connection.commit()
+print('+++++ Changes comitted to DB. +++++')  
+
+# close connection
 connection.close()
+print ('+++++ Connection to DB closed. +++++')
 
