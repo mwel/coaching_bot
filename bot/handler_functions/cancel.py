b31f0981ad0a238b1d62cf9b9bfb1493d82ae9a1
mwel <max.wellenhofer@gmail.com>
2022-01-06 20:11:29 +0100

    remove print statements for better cmd line output

--- bot/handler_functions/cancel.py
+++ bot/handler_functions/cancel.py
@@ -7,14 +7,21 @@ from telegram.ext import (
 from logEnabler import logger;
 
 
+from handler_functions.database_connector import delete_record_db
+
+
 # Cancels and ends the conversation.
 def cancel(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info(f'User {user.first_name} canceled the conversation.')
+
+    # delete record of user upon cancellation of the sign up process
+    delete_record_db(update.message.from_user.id)
+
     update.message.reply_text(
         f'APPLICATION TERMINATED by {user.first_name}\n\n'
         'You ended the converstation.'
-        # TODO: 'All your previously submitted data has been deleted.' 
+        'All your previously submitted data has been deleted.\n\n' 
         'Now you can close this chat -OR- /start over.',
         reply_markup=ReplyKeyboardRemove(),
     )
--- bot/handler_functions/database_connector/delete_record_db.py
+++ bot/handler_functions/database_connector/delete_record_db.py
@@ -3,11 +3,9 @@ import sqlite3
 def delete_record(user_id):
     # connect to db
     connection = sqlite3.connect("coachingBotDB.db")
-    print("+++++ Connected to coachingBotDB. +++++")
 
     # cursor
     cursor = connection.cursor()
-    print("+++++ Cursor created. +++++")
     
     # DELETE record for specific user_id
     cursor.execute(f'''DELETE FROM coachingBotDB.users WHERE user_id = {user_id};''')
@@ -15,8 +13,6 @@ def delete_record(user_id):
 
     #commit changes to db			
     connection.commit()
-    print('+++++ COMMITTED changes to DB. +++++')  
 
     # close connection
     connection.close()
-    print ('+++++ CLOSED connection to DB. +++++')
