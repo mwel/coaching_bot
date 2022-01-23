a85a15e2854166b0de5bb03a9c102aec6adaa050
mwel <max.wellenhofer@gmail.com>
2022-01-23 17:30:24 +0100

    reset quickstart file for Google Cal API

--- quickstart.py
+++ quickstart.py
@@ -29,7 +29,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'bot/constants/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
