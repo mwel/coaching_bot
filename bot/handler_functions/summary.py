f7cd604c38a8b940639fa236d2c2ce326c47fb85
mwel <max.wellenhofer@gmail.com>
2022-02-05 10:50:45 +0100

    update calendar functions (import error with "requests")

--- bot/handler_functions/summary.py
+++ bot/handler_functions/summary.py
@@ -14,7 +14,7 @@ from handler_functions.database_connector import select_db
 from handler_functions.confirmation_mail import confirmation_mail
 from handler_functions.calendar.generate_ics import generate_ics
 from handler_functions.calendar.send_invitation import send_invitation
-from appointments import make_appointment
+from calendar import make_appointment
 
 
 # Stores the photo and asks for a location.
