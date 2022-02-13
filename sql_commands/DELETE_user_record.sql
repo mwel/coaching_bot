d384c536da1b90518c557c99326e80ce0d28acdc
mwel <max.wellenhofer@gmail.com>
2022-02-13 19:14:05 +0100

    update thesis, delete unneccessary files, update glossar

--- sql_commands/DELETE_user_record.sql
+++
@@ -1,6 +0,0 @@
--- DELETE a specific user record
-DELETE FROM users WHERE user_id = user_id%;
-
-
--- DELETE all user records, but leave table structure intact >> commented out for safety reasons
--- DELETE FROM users;
--- sql_commands/SELECT_user_data.sql
+++
@@ -1,3 +0,0 @@
--- SELECT all informatione available from a single user
-
-SELECT * FROM users WHERE user_id = user_id%;
