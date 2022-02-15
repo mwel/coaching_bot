120a519b6f03e8c1beac47ca6b4f15eb645bf924
mwel <max.wellenhofer@gmail.com>
2022-02-15 11:07:13 +0100

    update mock-up credentials

---
+++ bot/constants_/MAIL_constant_.py
@@ -0,0 +1,14 @@
+MAIL_ACCOUNT_PW = "**********"
+
+# Mail Server IN (IMAP)
+post_in_server = 'www.mail_server.domain'
+mail_user_in = 'incoming@mailaddress_domain'
+port = 993
+encryption = 'SSL/TLS'
+
+# Mail Server OUT (IMAP)
+post_out_server = 'www.mail_server.domain'
+authentification = True
+mail_user_out = 'outgoing@mailaddress_domain'
+port = 465
+encryption = 'SSL/TLS'
