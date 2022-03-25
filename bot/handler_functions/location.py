d7fd78ffa5be78c072d30f07e88a0aa83e042c12
mwel <max.wellenhofer@gmail.com>
2022-03-25 12:20:31 +0100

    fix location.py for photo delivery (complete)

--- bot/handler_functions/location.py
+++ bot/handler_functions/location.py
@@ -1,7 +1,7 @@
 """ location handler function. called, when user arrives at location state """
 
 # imports
-from telegram import Update, ReplyKeyboardRemove, InputMediaPhoto
+from telegram import Update, ReplyKeyboardRemove, Bot
 from telegram.ext import CallbackContext
 from logEnabler import logger;
 
@@ -13,14 +13,16 @@ import os
 
 
 # build the path to the photo to be sent to the user
-# directory = os.path.join(str(Path(__file__).parent), 'ressources', 'cyberpunk.jpg')
-directory = 'https://github.com/mwel/coaching_bot/tree/main/bot/ressources/cyberpunk.jpg'
+directory = os.path.join(str(Path(__file__).parent.parent), 'ressources', 'cyberpunk.jpg')
+# print(f'>>>>>>>>>>>>> Photo Directory: {directory}')
+# directory = 'https://github.com/mwel/coaching_bot/blob/main/bot/ressources/cyberpunk.jpg'
 
 
 # Stores the information received and continues on to the next state
 def location(update: Update, context: CallbackContext) -> int:
     
     user_id = update.message.from_user.id
+    chat_id=update.effective_user.id
     user_location = update.message.location
     
     logger.info(f'+++++ Location of user {user_id}: {user_location.latitude} , {user_location.longitude} +++++')
@@ -41,8 +43,8 @@ def location(update: Update, context: CallbackContext) -> int:
         reply_markup=states.KEYBOARD_MARKUPS[states.PHOTO],
         )
 
-    # send the picture to the user
-    context.bot.send_photo(user_id, directory)
+    # send userpic to the user
+    context.bot.send_photo(chat_id=chat_id, photo=open(directory, 'rb'))
 
     
     # save state to DB
@@ -54,6 +56,7 @@ def location(update: Update, context: CallbackContext) -> int:
 def skip_location(update: Update, context: CallbackContext) -> int:
     
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
 
     logger.info(f'00000 No location submitted by {user_id}. 00000')
 
@@ -67,8 +70,8 @@ def skip_location(update: Update, context: CallbackContext) -> int:
         reply_markup=states.KEYBOARD_MARKUPS[states.PHOTO],
         )
 
-    # send the picture to the user
-    context.bot.send_photo(user_id, directory)
+    # send userpic to the user
+    context.bot.send_photo(chat_id=chat_id, photo=open(directory, 'rb'))
 
 
     # save state to DB
